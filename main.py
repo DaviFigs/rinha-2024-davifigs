d46e87b319db4074d7e28eaae2cfe9bf06b9a412
Davi Figueiredo <devdeoliveira06@gmail.com>
2024-02-17 14:46:55 -0400

    server api1 is running

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+
+app.post('clientes/{id}/transacoes')
+def transacoes(id:int, valor:int, tipo:str, descricao:str):
+    pass
+
+app.get('clientes/{id}/extrato')
+def extrato(id:int):
+    pass
